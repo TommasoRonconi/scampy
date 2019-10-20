7ac502146d756bc1715f4477eebb696cdc4c5f8d
Tommaso Ronconi <tommaso.ronconi@outlook.it>
2019-10-20 12:55:02 +0200

    tinker10 occupation, in Nsat: from M_min to 2 * M_min

--- halo_model/src/tinker10_p.cpp
+++ halo_model/src/tinker10_p.cpp
@@ -14,7 +14,7 @@ double sico::tinker10_p::Ncen ( const double Mhalo ) {
 
 double sico::tinker10_p::Nsat ( const double Mhalo ) {
 
-  const double Pc = PP( Mhalo, A_min, sigma_logA );
+  const double Pc = PP( Mhalo, 2. * A_min, sigma_logA );
   const double Ns = Pc * std::pow( ( Mhalo / A_sat ), alpha_sat );
   
   return ( Ns < 0 || std::isnan( Ns ) ) ? 0. : Ns;
