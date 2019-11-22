9db0da7c13caee39646674f5635efc1b7f5f7765
Tommaso Ronconi <tommaso.ronconi@outlook.it>
2019-11-22 11:11:35 +0100

    started porting documentation to readthedocs style in mkdocs enviroment

--- doc/sphinx/source/conf.py
+++ doc/sphinx/source/conf.py
@@ -14,15 +14,37 @@
 # import sys
 # sys.path.insert(0, os.path.abspath('.'))
 
+# -- Configuration for ReadTheDocs setup -------------------------------------
+
+import subprocess, os
+ 
+# def configureDoxyfile(input_dir, output_dir):
+#     with open('../../doxyfile.in', 'r') as file :
+#         filedata = file.read()
+ 
+#     filedata = filedata.replace('@DOXYGEN_INPUT_DIR@', input_dir)
+#     filedata = filedata.replace('@DOXYGEN_OUTPUT_DIR@', output_dir)
+ 
+#     with open('doxyfile', 'w') as file:
+#         file.write(filedata)
+ 
+# Check if we're running on Read the Docs' servers
+# read_the_docs_build = os.environ.get('READTHEDOCS', None) == 'True'
+ 
+breathe_projects = {}
+ 
+# if read_the_docs_build:
+#     input_dir = '../../../mbh'
+#     output_dir = 'build'
+#     configureDoxyfile(input_dir, output_dir)
+#     subprocess.call('doxygen', shell=True)
+#     breathe_projects['MBH'] = output_dir + '/xml'
 
 # -- Project information -----------------------------------------------------
 
-project = 'SCAMMER'
+project = 'ScamPy'
 copyright = '2019, Tommaso Ronconi'
 author = 'Tommaso Ronconi'
-
-# The full version, including alpha/beta/rc tags
-release = '0.1.0'
 
 
 # -- General configuration ---------------------------------------------------
@@ -30,9 +52,10 @@ release = '0.1.0'
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = [
-    'sphinx.ext.autodoc'
-]
+extensions = [ 'breathe' ]
+
+# Breathe configuration
+breathe_default_project = 'ScamPy'
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
@@ -48,7 +71,6 @@ exclude_patterns = []
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-# html_theme = 'alabaster'
 html_theme = 'sphinx_rtd_theme'
 
 # Add any paths that contain custom static files (such as style sheets) here,
---
+++ doc/sphinx/source/index.rst
@@ -0,0 +1,35 @@
+.. MBH documentation master file, created by
+   sphinx-quickstart on Fri Nov  8 10:04:42 2019.
+   You can adapt this file completely to your liking, but it should at least
+   contain the root `toctree` directive.
+
+Welcome to ScamPy's documentation!
+===============================
+
+.. toctree::
+   :maxdepth: 2
+   :caption: Contents:
+
+
+
+Indices and tables
+==================
+
+* :ref:`genindex`
+* :ref:`modindex`
+* :ref:`search`
+
+Docs
+====
+
+.. doxygenstruct:: cosmology
+   :members:
+      
+.. doxygenstruct:: interpolator
+   :members:
+      
+.. doxygenstruct:: halo_model
+   :members:
+      
+.. doxygenstruct:: cross_halo_model
+   :members:
