e81350034cf93d985d500f9a2854f3e4f1e2f7f0
Tommaso Ronconi <tommaso.ronconi@outlook.it>
2020-02-18 11:21:48 +0100

    fixed abundance_matching() function, generalized, tested for several redshifts and observables, authomatized where possible

--- python/scampy/abundance_matching.py
+++ python/scampy/abundance_matching.py
@@ -43,6 +43,7 @@ def abundance_matching ( gxy_array, lum_func, minL = -30, maxL = -15,
                          minP = 1.e-20, maxP = 1., nbinL = 200, factL = 1.,
                          minM = None, maxM = None, nbinM = 10, factM = 1. ) :
 
+    # some checks and set-ups:
     if nbinL < 3 :
         raise Exception( 'nbinL should not be smaller than 3, you gave {:d}'.format( nbinL ) )
     masses = numpy.array( [ gxy.mass for gxy in gxy_array ] )
@@ -66,79 +67,40 @@ def abundance_matching ( gxy_array, lum_func, minL = -30, maxL = -15,
     # Get cumulative sub-halo mass function
     lminM = numpy.log10( minM )
     lmaxM = numpy.log10( maxM )
+    invbinM = ( nbinM - 1 ) / ( lmaxM - lminM )
     MM = numpy.logspace( lminM, lmaxM, nbinM )
-    invbinM = 1. / ( numpy.log10( MM[ 1 ] ) - numpy.log10( MM[ 0 ] ) )
     dN, _ = cumulative_counts( numpy.array( [ gxy.mass for gxy in gxy_array ] ), MM, int( 1 ) )
     dN *= factM
 
+    if dN.min() < numpy.min( dPhi[ wP ] ) : 
+        raise Exception( 'Minimum cumulative number of objects ({0:.3e}) is smaller than minimum probability ({1:.3e})'.format( numpy.min( dN ),
+                                                                                                                                numpy.min( dPhi[ wP ] ) )  )
+    if dN.max() > numpy.max( dPhi[ wP ] ) :
+        raise Exception( 'Maximum cumulative number of objects ({0:.3e}) is greater than maximum probability ({1:.3e})'.format( numpy.max( dN ),
+                                                                                                                                numpy.max( dPhi[ wP ] ) )  )    
     # Variational bin matching
     for obj in gxy_array :
-        idx = int( ( numpy.log10( obj.mass ) - lminM ) * invbinM ) + 1
+
+        # get index of higher probability:
+        idx = int( ( numpy.log10( obj.mass ) - lminM ) * invbinM )
+        
+        # this exception here might be useless:
         if ( idx < 0 ) | ( nbinM <= idx ) :
             raise Exception( 'Index {0:d} is out of bounds. This galaxy has mass {1:.3e} Msol/h.'.format( idx, obj.mass ) )
-        Nl = dN[ idx ]
-        Nh = dN[ idx - 1 ]
-        if ( invPhi_f.get_xmin() < Nl ) & ( Nh < invPhi_f.get_xmax() ) :
-            L_low = invPhi_f( Nh )
-            L_high = invPhi_f( Nl )
-            obj.luminosity = numpy.random.uniform( L_low, L_high )
+
+        # if last bin, set to minimum probability:
+        if idx + 1 != nbinM :
+            Nl = dN[ idx + 1 ]
+        else :
+            Nl = invPhi_f.get_xmin()
+
+        # get higher probability bin:
+        Nh = dN[ idx ]
+
+        # get random value between limits of binned observables: 
+        L_low = invPhi_f( Nh )
+        L_high = invPhi_f( Nl )
+        obj.luminosity = numpy.random.uniform( L_low, L_high )
 
 
     return gxy_array
-
-# def abundance_matching ( gxy_array, lum_func, minL = -30, maxL = -15,
-#                          minP = 1.e-20, maxP = 1., nbinL = 200, factL = 1.,
-#                          minM = None, maxM = None, nbinM = 10, factM = 1. ) :
-
-#     if nbinL < 3 :
-#         raise Exception( 'nbinL should not be smaller than 3, you gave {:d}'.format( nbinL ) )
-#     masses = numpy.array( [ gxy.mass for gxy in gxy_array ] )
-#     if minM is None :
-#         minM = numpy.min( masses )
-#         minM -= 0.01 * minM
-#     if maxM is None :
-#         maxM = numpy.max( masses )
-#         maxM -= 0.01 * maxM
-    
-#     # Get cumulative luminosity function
-#     LL = numpy.linspace( minL, maxL, nbinL )
-#     dPhi = cumulative_from_differential( lum_func, LL, factL )
-
-#     # Get limits for inversion
-#     wP = numpy.where( [ ( minP <= _p ) & ( _p <= maxP ) for _p in dPhi ] )
-
-#     # Get inverse dPhi 
-#     invPhi_f = lin_interpolator( dPhi[ wP ], LL[ wP ] )
-
-#     # Get cumulative sub-halo mass function
-#     lminM = numpy.log10( minM )
-#     lmaxM = numpy.log10( maxM )
-#     invbinM = ( nbinM - 1 ) / ( lmaxM - lminM )
-#     MM = numpy.logspace( lminM, lmaxM, nbinM )
-#     # invbinM = 1. / ( numpy.log10( MM[ 1 ] ) - numpy.log10( MM[ 0 ] ) )
-#     dN, _ = cumulative_counts( numpy.array( [ gxy.mass for gxy in gxy_array ] ), MM, int( 1 ) )
-#     # if numpy.where( dN == 0 )[ 0 ].shape[ 0 ] > 0 :
-#     #     raise Exception( 'Zero values encountered in number counts.' )
-#     dN *= factM
-
-#     if numpy.min( dN ) < numpy.min( dPhi[ wP ] ) : 
-#         raise Exception( 'Minimum cumulative number of objects ({0:.3e}) is smaller than minimum probability ({1:.3e})'.format( numpy.min( dN ),
-#                                                                                                                                 numpy.min( dPhi[ wP ] ) )  )
-#     if numpy.max( dN ) > numpy.max( dPhi[ wP ] ) :
-#         raise Exception( 'Maximum cumulative number of objects ({0:.3e}) is greater than maximum probability ({1:.3e})'.format( numpy.max( dN ),
-#                                                                                                                                 numpy.max( dPhi[ wP ] ) )  )
-
-#     # Variational bin matching
-#     for obj in gxy_array :
-#         idx = int( ( numpy.log10( obj.mass ) - lminM ) * invbinM ) 
-#         if ( idx < 0 ) | ( nbinM <= idx ) :
-#             raise Exception( 'Index {0:d} is out of bounds. This galaxy has mass {1:.3e} Msol/h.'.format( idx, obj.mass ) )
-#         Nl = dN[ idx ]
-#         Nh = dN[ idx - 1 ]
-#         if ( invPhi_f.get_xmin() < Nl ) & ( Nh < invPhi_f.get_xmax() ) :
-#             L_low = invPhi_f( Nh )
-#             L_high = invPhi_f( Nl )
-#             obj.luminosity = numpy.random.uniform( L_low, L_high )
-
-
-#     return gxy_array
