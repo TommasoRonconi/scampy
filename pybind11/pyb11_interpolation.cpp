c91bbd50005847f0e9e6891d15906ddbb6c0af4b
Tommaso Ronconi <tommaso.ronconi@outlook.it>
2023-04-03 21:51:56 +0100

    added log_interp but I didn't quite figured out if it is buggy or not yet

--- pybind11/pyb11_interpolation.cpp
+++ pybind11/pyb11_interpolation.cpp
@@ -7,6 +7,7 @@
 namespace py = pybind11;
 
 template class utl::interpolator< utl::lin_interp >;
+template class utl::interpolator< utl::log_interp >;
 
 PYBIND11_MODULE( interpolation, m ) {
   
@@ -17,6 +18,25 @@ PYBIND11_MODULE( interpolation, m ) {
     .def("__call__", py::vectorize(&utl::interpolator< utl::lin_interp >::operator()),
 	 "Evaluate the interpolated function", py::arg("x") )
     .def("integrate", &utl::interpolator< utl::lin_interp >::integrate,
+	 "Integrate the interpolated function within a specified interval.\n"
+	 "\nParameters"
+	 "\n----------"
+	 "\naa : float"
+	 "\n\tinterval lower limit"
+	 "\nbb : float"
+	 "\n\tinterval upper limit\n"
+	 "\nReturns"
+	 "\n------"
+	 "\n: float"
+	 "\n\tapproximate integral", py::arg("aa"), py::arg("bb") );
+  
+  py::class_< utl::interpolator< utl::log_interp > >( m, "log_interp" )
+    .def(py::init< const std::vector< double > &, const std::vector< double > & >())
+    .def("get_x", &utl::interpolator< utl::log_interp >::get_xv )
+    .def("get_y", &utl::interpolator< utl::log_interp >::get_fv )
+    .def("__call__", py::vectorize(&utl::interpolator< utl::log_interp >::operator()),
+	 "Evaluate the interpolated function", py::arg("x") )
+    .def("integrate", &utl::interpolator< utl::log_interp >::integrate,
 	 "Integrate the interpolated function within a specified interval.\n"
 	 "\nParameters"
 	 "\n----------"
