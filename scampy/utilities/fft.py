aa8b2cc8278ff97e8a8d00b19a71d60e4d57f25b
Tommaso Ronconi <tommaso.ronconi@outlook.it>
2023-04-05 16:43:25 +0100

    added new module in sub-package scampy.utilities computing FFTs using scipy.fft library. Currently implemented: Fast Sin-Transform Log

---
+++ scampy/utilities/fft.py
@@ -0,0 +1,50 @@
+""" 
+"""
+
+#############################################################################################
+
+# External imports
+from scipy import fft
+import numpy
+
+#############################################################################################
+
+# Fast sin-transform log (direct)
+def fstl ( xn, yn, lk0 = 0.0, bias = 0.0, mu = 0.5 ) :
+    
+    # log and copy input
+    lxn = numpy.log( xn )
+    
+    # Find log-spacing and check equal
+    dlr = None
+    try :
+        # Note that the `numpy.around` function is less
+        # accurate than the equivalent python built-in 
+        # function `round` 
+        # (rounds to the closest machine precision float)
+        dlr = numpy.unique( 
+            numpy.around( lxn[1:]-lxn[:-1], 4 ) 
+        ).item()
+    except :
+        raise
+        
+    # Prepare input function for transform
+    fct = 0.5 * numpy.sqrt( 0.5 * numpy.pi ) / numpy.pi**2
+    an  = numpy.array( fct * yn * xn * numpy.sqrt( xn ) )
+        
+    # Find optimal centre for transform
+    kr = fft.fhtoffset( dlr, initial = lk0, 
+                        mu = mu, bias = bias )
+    
+    # Compute wavenumber grid
+    kn = numpy.exp( kr - lxn[::-1] )
+    
+    # Compute sin transform of an
+    bn = fft.fht( an, dlr, mu = mu, bias = bias, offset = kr )
+    
+    # Return a tuple with
+    # - wavenumber grid
+    # - converted sin-transform of input function
+    return kn, bn / ( kn * numpy.sqrt( kn ) )
+
+#############################################################################################
