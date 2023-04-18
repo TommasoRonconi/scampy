1b7f3d2f0bfb52ead9b3ef8e0aa21c15fe7ef85e
Tommaso Ronconi <tommaso.ronconi@outlook.it>
2023-04-18 12:08:56 +0100

    added function rejection_sampling

--- scampy/utilities/functions.py
+++ scampy/utilities/functions.py
@@ -88,3 +88,91 @@ def trap_int ( xx, yy ) :
     return numpy.sum( 0.5 * ( yy[:-1] + yy[1:] ) * ( xx[1:] - xx[:-1] ), axis = 0 )
 
 ############################################################################################
+
+def rejection_sampling ( pdf, 
+                         xmin = 0.0, xmax = 1.0, 
+                         umin = 0.0, umax = 1.0,
+                         logu = False,
+                         size = 1, rng = None, 
+                         verbose = False
+                       ) :
+    """Samples the ``X`` random variable from a custom target PDF using rejection sampling.
+    Proposals for the random variable are drawn from a uniform distribution defined between 
+    (xmin, xmax).
+    A proposal ``x_j`` is accepted if the corresponding value of the proposal PDF ``u_j``
+    is lower than the value of the target PDF computed at the proposal random variable value, 
+    i.e. if ``u_j < PDF(x_j)``.
+    Values of the proposal PDF are drawn from a uniform distribution defined between (umin, umax).
+    The higher the value of umax with respect to the maximum of the target PDF, the lower
+    the acceptance fraction (but a higher value of umax might produce more reliable samples).
+    
+    Parameters
+    ----------
+    pdf : callable
+        the callable function defining the custom PDF (it must have a ``__call__`` method)
+    xmin : float
+        (Optional, default = 0.0) minimum value of the random variable X
+    xmax : float
+        (Optional, default = 1.0) maximum value of the random variable X
+    umin : float
+        (Optional, default = 0.0) minimum value of the proposal PDF 
+    umax : float
+        (Optional, default = 1.0) maximum value of the proposal PDF
+    logu : bool
+        (Optional, default = False) if True, ``umin`` and ``umax`` are interpreted 
+        as log10(umin) and log10(umax)
+    size : int
+        (Optional, default = 1) size of the output sample
+    rng : int or random generator
+        (Optional, default = None) either an integer or a random number generator.
+        If an integer is passed, a generator will be instantiated with ``seed=rng``.
+        If None is passed, a random number generator will be instantiated 
+        with randomly generated seed.
+    verbose : bool
+        (Optional, default = False) if True prints on stdout informations about the sampling
+    
+    Returns
+    -------
+    : 1d-array
+        A 1D array with ``size`` samples of the random variable extracted from the target PDF
+        
+    Note
+    ----
+    One can run the Kolmogorov-Smirnov Test to verify that the sampled random variable has
+    a PDF that approximates the target PDF.
+    """
+    
+    if not hasattr( pdf, '__call__' ) :
+        raise ValueError( 'pdf argument should be callable' )
+    
+    if rng is None :
+        rng = numpy.random.default_rng()
+    elif isinstance( rng, int ) :
+        rng = numpy.random.default_rng( seed = rng )
+    elif not isinstance( rng, numpy.random.Generator ) :
+        raise ValueError( 'rng argument should be one among None, integer, random generator')
+    
+    sample = numpy.zeros( size )
+    notassigned = numpy.ones_like( sample, dtype=bool )
+    it = 0
+    if verbose : acceptfrac = 0.0
+    while notassigned.any() :
+        x = rng.uniform( low = xmin, high = xmax, size = size )
+        u = rng.uniform( low = umin, high = umax, size = size )
+        if logu : u = 10**u
+        assign = ( u < pdf(x) )
+        sample[assign] = x[assign]
+        notassigned &= ~assign
+        it += 1
+        if verbose :
+            acceptfrac += assign.sum()/assign.size
+            print(f'Assigned {100*(~notassigned).sum()/notassigned.size:.2f} %', end='\r')
+    if verbose : 
+        print(
+            f'\nDone in {it} iterations'
+            f'\nAverage percent acceptance: {100*acceptfrac/it:.2f}%'
+         )
+    
+    return sample
+
+############################################################################################
