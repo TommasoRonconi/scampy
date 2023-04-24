267adb9781f8b83473a6f9d3c3497b3c99607575
Tommaso Ronconi <tommaso.ronconi@outlook.it>
2023-04-24 09:53:42 +0100

    added fast projected sin-transform log

--- scampy/utilities/fft.py
+++ scampy/utilities/fft.py
@@ -48,3 +48,43 @@ def fstl ( xn, yn, lk0 = 0.0, bias = 0.0, mu = 0.5 ) :
     return kn, bn / ( kn * numpy.sqrt( kn ) )
 
 #############################################################################################
+
+# Fast projected sin-transform log (direct)
+def fpstl ( xn, yn, lk0 = 0.0, bias = 0.0, mu = 0.0 ) :
+    
+    # log and copy input
+    lxn = numpy.log( xn )
+    
+    # Find log-spacing and check equal
+    dlr = None
+    try :
+        # Note that the `numpy.around` function is less
+        # accurate than the equivalent python built-in 
+        # function `round` 
+        # (rounds to the closest machine precision float)
+        dlr = numpy.unique( 
+            numpy.around( lxn[1:]-lxn[:-1], 4 ) 
+        ).item()
+    except :
+        raise
+        
+    # Prepare input function for transform
+    fct = 0.5  / numpy.pi
+    an  = numpy.array( fct * yn * xn )
+        
+    # Find optimal centre for transform
+    kr = fft.fhtoffset( dlr, initial = lk0, 
+                        mu = mu, bias = bias )
+    
+    # Compute wavenumber grid
+    kn = numpy.exp( kr - lxn[::-1] )
+    
+    # Compute sin transform of an
+    bn = fft.fht( an, dlr, mu = mu, bias = bias, offset = kr )
+    
+    # Return a tuple with
+    # - wavenumber grid
+    # - converted sin-transform of input function
+    return kn, bn / kn
+
+#############################################################################################
