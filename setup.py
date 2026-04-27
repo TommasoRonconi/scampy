10296015dc197ef8dcce081a5a8982e1f3e00061
Tommaso Ronconi <tommaso.ronconi@outlook.it>
2026-04-27 20:33:50 +0200

    removed trailing spaces

--- setup.py
+++ setup.py
@@ -10,12 +10,12 @@ def main () :
     setup( name = "scampy",
            version = "2.0.0",
            package_dir = {
-               'scampy'              : 'scampy',
-               'scampy.io'          : os.path.join( 'scampy', 'io' ),
-               'scampy.halo'        : os.path.join( 'scampy', 'halo' ),
-               'scampy.measure'     : os.path.join( 'scampy', 'measure' ),
-               'scampy.plot'        : os.path.join( 'scampy', 'plot' ),
-               'scampy.utilities'   : os.path.join( 'scampy', 'utilities' )
+               'scampy'            : 'scampy',
+               'scampy.io'        : os.path.join( 'scampy', 'io' ),
+               'scampy.halo'      : os.path.join( 'scampy', 'halo' ),
+               'scampy.measure'   : os.path.join( 'scampy', 'measure' ),
+               'scampy.plot'      : os.path.join( 'scampy', 'plot' ),
+               'scampy.utilities' : os.path.join( 'scampy', 'utilities' )
            },
            packages = [ 'scampy',
                         'scampy.io',
