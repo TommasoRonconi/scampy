9d07bb2f5aa8afd3ada245d3883911939746e9a4
Tommaso Ronconi <tommaso.ronconi@outlook.it>
2026-04-27 12:42:28 +0200

    completed migration to pure-python in build-system

--- setup.py
+++ setup.py
@@ -1,73 +1,21 @@
 ########################################################################################
 
-import os, sys
-from glob import glob
-from setuptools import setup
-from pybind11.setup_helpers import Pybind11Extension, build_ext
+import os
+from setuptools import setup, find_packages
 
 ########################################################################################
 
 def main () :
 
-    extra_OMP_compile_args = ['-fopenmp']
-    extra_OMP_link_args = ['-fopenmp']
-    if sys.platform == 'darwin' :
-        extra_OMP_compile_args += [  '-I/usr/local/opt/libomp/include', '-Xpreprocessor' ]
-        extra_OMP_link_args += [ '-L/usr/local/opt/libomp/lib' ]
-
-    ####################################################################################
-    # Interpolation extension, currently providing the linear interpolator
-    intrp_ext = Pybind11Extension(
-        "scampy.utilities.interpolation",
-        sorted(
-            [ os.path.join( 'pybind11', 'pyb11_interpolation.cpp' ) ]
-        ),
-        include_dirs = sorted( [ os.path.join( 'c++', 'utilities', 'include' ),
-                                 os.path.join( 'c++', 'interpolator', 'include' ) ] ),
-        extra_compile_args=[ '-std=c++17' ]
-    )
-
-    ####################################################################################
-    # Cosmology extension, provides mainly cosmographic functions and functions
-    # depending on the parameters and Friedmann equations
-    cosmo_ext = Pybind11Extension(
-        "scampy.cosmology",
-        sorted(
-            [ os.path.join( 'pybind11', 'pyb11_cosmology.cpp' ),
-              os.path.join( 'c++', 'cosmology', 'src', 'cosmological_model.cpp' ) ]
-        ),
-        include_dirs = sorted( [ os.path.join( 'c++', 'utilities', 'include' ),
-                                 os.path.join( 'c++', 'interpolator', 'include' ),
-                                 os.path.join( 'c++', 'cosmology', 'include' ) ] ),
-        extra_compile_args=[ '-std=c++17' ]
-    )
-
-    ####################################################################################
-    # Clustering extension providing compiled and OMP-parallel
-    # functions to compute distances
-    clust_ext = Pybind11Extension(
-        "scampy.measure.clustering_core",
-        sorted(
-            [ os.path.join( 'pybind11', 'pyb11_clustering_core.cpp' ),
-              os.path.join( 'c++', 'utilities', 'src', 'clustering_core.cpp' ) ]
-        ),
-        include_dirs = sorted( [ os.path.join( 'c++', 'utilities', 'include' ) ] ),
-        libraries = [ "m", "gomp" ],
-        extra_compile_args=['-std=c++17'] + extra_OMP_compile_args,
-        extra_link_args=extra_OMP_link_args
-    )
-
-    ####################################################################################
-    # Run setup
     setup( name = "scampy",
            version = "2.0.0",
            package_dir = {
-               'scampy' : 'scampy',
-               'scampy.io' : os.path.join( 'scampy', 'io' ),
-               'scampy.halo' : os.path.join( 'scampy', 'halo' ),
-               'scampy.measure' : os.path.join( 'scampy', 'measure' ),
-               'scampy.plot' : os.path.join( 'scampy', 'plot' ),
-               'scampy.utilities' : os.path.join( 'scampy', 'utilities' )
+               'scampy'              : 'scampy',
+               'scampy.io'          : os.path.join( 'scampy', 'io' ),
+               'scampy.halo'        : os.path.join( 'scampy', 'halo' ),
+               'scampy.measure'     : os.path.join( 'scampy', 'measure' ),
+               'scampy.plot'        : os.path.join( 'scampy', 'plot' ),
+               'scampy.utilities'   : os.path.join( 'scampy', 'utilities' )
            },
            packages = [ 'scampy',
                         'scampy.io',
@@ -76,16 +24,10 @@ def main () :
                         'scampy.plot',
                         'scampy.utilities'
            ],
-           cmdclass={"build_ext": build_ext},
-           ext_modules = [ intrp_ext, cosmo_ext, clust_ext ],
-           setup_requires = [ 'setuptools', 'pybind11' ],
+           setup_requires = [ 'setuptools' ],
            install_requires = [ 'numpy', 'scipy', 'matplotlib', 'h5py' ]
     )
 
 ########################################################################################
 
 if __name__ == "__main__" : main()
-
-
-
-    
